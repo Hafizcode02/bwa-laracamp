89b2e1447e72eee778b636d0dd6effeb144d516b
HafiCode02 <hafizcode02@gmail.com>
2022-03-03 10:38:56 +0700

    create camp_benefit seeder

--- database/seeders/CampBenefitSeeder.php
+++ database/seeders/CampBenefitSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\CampBenefit;
 use Illuminate\Database\Seeder;
 
 class CampBenefitSeeder extends Seeder
@@ -13,6 +14,53 @@ class CampBenefitSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $camp_benefits = [
+            [
+                'camp_id' => 1,
+                'name' => 'Pro Techstack Kit'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => '1-1 Mentoring Program'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Final Project Certificate'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Offline Course Videos'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Future Job Opportinity'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Premium Design Kit'
+            ],
+            [
+                'camp_id' => 1,
+                'name' => 'Website Builder'
+            ],
+            [
+                'camp_id' => 2,
+                'name' => '1-1 Mentoring Program'
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Final Project Certificate'
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Offline Course Videos'
+            ],
+            [
+                'camp_id' => 2,
+                'name' => 'Premium Design Kit'
+            ],
+        ];
+
+        CampBenefit::insert($camp_benefits);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CampSeeder::class,
+            CampBenefitSeeder::class,
         ]);
     }
 }
